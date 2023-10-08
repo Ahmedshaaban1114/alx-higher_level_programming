fa9e055fc431f42862c30d32c7d7e418b5a6c3cc
Ahmedshaaban1114 <Ahmed@example.com>
2023-10-07 17:55:11 -0700

    F

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+def no_c(my_string):
+    back = ""
+    for i in range(len(my_string)):
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
+            back += my_string[i]
+    return back
