3e48be36f0a55135cbbb1051d44e537e2920c3e4
Ahmedshaaban1114 <Ahmed@example.com>
2023-10-16 09:04:33 -0700

    F

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    if  a_dictionary is None:
-        return
-    for k in sorted(a_dictionary.keys()):
-        print("{} : {}".format(k, a_dictionary.get(k)))
-
 def update_dictionary(a_dictionary, key, value):
     n_dic = {key: value}
     a_dictionary.update(n_dic)
-    return a_dictionary
+    return (a_dictionary)
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    if  a_dictionary is None:
-        return
-    for k in sorted(a_dictionary.keys()):
-        print("{} : {}".format(k, a_dictionary.get(k)))
-
 def simple_delete(a_dictionary, key=""):
     if a_dictionary is None:
-        return
-    if key in a_dictionary:
+        return (0)
+    elif key in a_dictionary:
         del a_dictionary[key]
-    return a_dictionary
+    return (a_dictionary)
