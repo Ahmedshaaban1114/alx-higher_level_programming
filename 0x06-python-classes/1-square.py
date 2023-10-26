cff077e80a137f84fae13e7ac779b3fcf255fd8d
Ahmedshaaban1114 <Ahmed@example.com>
2023-10-25 18:37:45 -0700

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Square"""
+
+class square:
+
+    def __int__(self, size):
+
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+
+    def __int__(self, size):
+
+        if not isinstance (size, int):
+            raise TypeError ('size must be an integar')
+        if size < 0:
+            raise ValueError ('size must be >= 0')
+        self.__size = size
