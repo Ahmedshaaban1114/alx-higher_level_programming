abb5e958319db0f53693ce5e2327230e0100be73
Ahmedshaaban1114 <Ahmed@example.com>
2024-02-29 17:00:21 +0000

    D

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,2 +1,3 @@
-ends a GET request to the URL, and displays the body of the response
+#!/bin/bash
+# Sends a GET request to the URL, and displays the body of the response
 curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,2 +1,3 @@
-ends a POST request to the passed URL, and displays the body of the response
+#!/bin/bash
+# Sends a POST request to the passed URL, and displays the body of the response
 curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" "${1}"
